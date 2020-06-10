4f4dd56f9f468a953ca6b1690ab679189cb414d4
Sujan Prajapati Maharjan <prajapati_sujan1@hotmail.com>
2020-06-10 23:37:59 +1000

    Add new Stun Server

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -6,6 +6,12 @@ const peerConnection=new RTCPeerConnection({
         },
         {
             urls: 'stun:stun1.l.google.com:19302'
+        },
+        {
+            urls: 'stun:stun2.l.google.com:19302'
+        },
+        {
+            urls: 'stun:stun3.l.google.com:19302'
         }
     ]
 });
